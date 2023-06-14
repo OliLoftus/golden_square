852cac5e966bae728c8c9472c7b79b22c4a084db
Oliver Loftus <oliverloftus1@gmail.com>
2023-06-14 11:18:59 +0100

    cypher debug complete

--- debugging/debug.cypher.py
+++ debugging/debug.cypher.py
@@ -1,43 +1,42 @@
 def encode(text, key):
     cipher = make_cipher(key)
-    print(f"c*cipher: {cipher}")
     ciphertext_chars = []
     for i in text:
         ciphered_char = chr(65 + cipher.index(i))
-        print(f"* cipher index {cipher.index(i)}")
-        print(f"* char 65 list: {ciphered_char}")
+        # iterates over text and for the char(i) in text find the index of that in the cipher of the key and
+        # adds it to 65(a) then appends to ciphered_chars
         ciphertext_chars.append(ciphered_char)
+    print(f"* ciphertext_chars: {ciphertext_chars}")
 
     return "".join(ciphertext_chars)
 
 
 def decode(encrypted, key):
     cipher = make_cipher(key)
-
+    print(f"* cipher: {cipher}")
     plaintext_chars = []
     for i in encrypted:
-        plain_char = cipher[65 - ord(i)]
+        print(f"* Index: {i}")
+        plain_char = cipher[abs(65 - ord(i))]
+        # discovered 65 - ord(i) was a negative value where it needed to be a positive so use the abs method to 
+        # ensure its the absolute value ignoring the negative sign
         plaintext_chars.append(plain_char)
 
     return "".join(plaintext_chars)
 
 
 def make_cipher(key):
-    alphabet = [chr(i + 98) for i in range(1, 26)]
-    print(f"* this is alphabet: {alphabet}")
+    alphabet = [chr(i + 96) for i in range(1, 27)]
+    # changed so it includes a(96 instead of 98) and includes z so 27 instead of 26
     cipher_with_duplicates = list(key) + alphabet
-    print(f"* value in key list is added to alphabet list: {cipher_with_duplicates}")
     cipher = []
     for i in range(0, len(cipher_with_duplicates)):
         if cipher_with_duplicates[i] not in cipher_with_duplicates[:i]:
-            print(f" * i: {cipher_with_duplicates[i]}")
-            print(f" * before i: {cipher_with_duplicates[:i]}")
             cipher.append(cipher_with_duplicates[i])
-            print(f"* appendend list: {cipher}")
     return cipher
 
-# When you run this file, these next lines will show you the expected
-# and actual outputs of the functions above.
+
+
 print(f"""
  Running: encode("theswiftfoxjumpedoverthelazydog", "secretkey")
 Expected: EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL
